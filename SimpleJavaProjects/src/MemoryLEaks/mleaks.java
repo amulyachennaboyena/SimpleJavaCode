6acb842c8f0f81a1eb7e49f0675fc94271479e9e
Amulya Chennaboyena <axc29990@ucmo.edu>
2018-05-14 09:24:18 -0500

    memory leaks

---
+++ SimpleJavaProjects/src/MemoryLEaks/mleaks.java
@@ -0,0 +1,40 @@
+/*
+ * Task3-MemoryLeaks
+	1. What are memory leaks?
+	memory leak in Java is a situation where some objects are not used 
+	by the application any more, but GC fails to recognize them as unused.
+	
+	2. Demonstrate memory leaks problem with an example.
+	
+	3. Use atleast one tool to detect memory leaks.
+ */
+
+
+package MemoryLEaks;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class mleaks {
+	private Map cache = new HashMap();
+	   public int square(int i) {
+	      int result = i * i;
+	      cache.put(i, result);
+	      return result;
+	   }
+	   public static void main(String[] args) {
+	      Scanner sc = new Scanner(System.in);
+	      int i = 0;
+		   
+		   mleaks calc = new mleaks();
+	      while (true) {
+	    	  
+	         System.out.println("Enter a number between 1 and 100");
+	     i = sc.nextInt();
+	   }
+	
+
+}
+}
+
